e2065e07a3586a882141568b0b262837f2b54f20
taavi80 <taavi.univer80@gmail.com>
2024-03-02 15:48:05 +0200

    update

--- meet-landing-page/astro.config.mjs
+++ meet-landing-page/astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: "https://github.com/taavi80/frontendmentor-challenges/meet-landing-page",
-  base: "/meet-landing-page",
+  base: "/frontendmentor-challenges",
 });
 
 
